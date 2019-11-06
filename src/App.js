f00324dce3bf022a3d95548fcd6df77a3ec0c9d8
fishmanjohn <john@emmaandjohnbyvan.com>
2019-11-06 13:21:48 -0800

    started file structure

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <section className ="header">
+
+      </section>
+      <section>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      </section>
+      
     </div>
   );
 }
